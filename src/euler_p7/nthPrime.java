7af04b35323d2458f3cac773fc49281d00d72919
Raphael Melo <raphaelmelo@Mac-mini-de-Raphael.local>
2014-07-09 21:24:50 -0300

    adas

--- src/euler_p7/nthPrime.java
+++ src/euler_p7/nthPrime.java
@@ -7,7 +7,7 @@ import Util.Util;
 
 public class nthPrime {
 
-	public static Long nthPrime(int n){
+	public static Long getNthPrime(int n){
 		List<Long> generatedPrimes = new ArrayList<Long>();
 		Long nthPrime = 2L;
 		generatedPrimes.add(nthPrime);
@@ -20,7 +20,7 @@ public class nthPrime {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(nthPrime(10001));
+		System.out.println(getNthPrime(10001));
 	}
 
 }
